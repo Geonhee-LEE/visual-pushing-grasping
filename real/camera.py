9aa0b729ecebfa65659f813bba5145c0f94c815a
Geonhee-LEE <gunhee6392@gmail.com>
2019-11-10 10:06:59 +0900

    Test numpy ndarray to img

--- real/camera.py
+++ real/camera.py
@@ -8,6 +8,110 @@ import time
 import struct
 
 
+import sys
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import threading
+
+class Camera(object):
+
+    def __init__(self):
+
+        # Data options (change me)
+        self.im_height = 720
+        self.im_width = 1280
+        self.tcp_host_ip = '127.0.0.1'
+        self.tcp_port = 50000
+        self.buffer_size = 4098 # 4 KiB
+
+        # Connect to server
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.connect((self.tcp_host_ip, self.tcp_port))
+
+        self.intrinsics = None
+        self.get_data()
+
+        # ROS Interfacing
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher("image_topic",Image)
+        
+        _thread = threading.Thread(target=self.imageCB)
+        _thread.daemon = True
+        _thread.start()
+
+
+    def imageCB(self):
+        rate = rospy.Rate(30) # 10hz
+        cv2.namedWindow("color_img",cv2.WINDOW_AUTOSIZE)
+        
+        while not rospy.is_shutdown():
+            rospy.loginfo("imageCB")
+            # Ping the server with anything
+            self.tcp_socket.send(b'asdf')
+
+            # Fetch TCP data:
+            #     color camera intrinsics, 9 floats, number of bytes: 9 x 4
+            #     depth scale for converting depth from uint16 to float, 1 float, number of bytes: 4
+            #     depth image, self.im_width x self.im_height uint16, number of bytes: self.im_width x self.im_height x 2
+            #     color image, self.im_width x self.im_height x 3 uint8, number of bytes: self.im_width x self.im_height x 3
+            data = b''
+            while len(data) < (10*4 + self.im_height*self.im_width*5):
+                data += self.tcp_socket.recv(self.buffer_size)
+
+            # Reorganize TCP data into color and depth frame
+            self.intrinsics = np.fromstring(data[0:(9*4)], np.float32).reshape(3, 3)
+            depth_scale = np.fromstring(data[(9*4):(10*4)], np.float32)[0]
+            depth_img = np.fromstring(data[(10*4):((10*4)+self.im_width*self.im_height*2)], np.uint16).reshape(self.im_height, self.im_width)
+            color_img = np.fromstring(data[((10*4)+self.im_width*self.im_height*2):], np.uint8).reshape(self.im_height, self.im_width, 3)
+            depth_img = depth_img.astype(float) * depth_scale
+
+            # Color np to img
+            tmp_color_data = np.asarray(color_img)
+            tmp_color_data.shape = (self.im_height,self.im_width,3)
+            tmp_color_image = cv2.cvtColor(tmp_color_data, cv2.COLOR_RGB2BGR)
+            #Displayed the image
+            cv2.imwrite(os.path.join('.', 'test.png'), tmp_color_image)
+            #cv2.imshow("depth_img", depth_img)
+
+            #Converting OpenCV images to ROS image messages
+            try:
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(tmp_color_image, "bgr8"))
+            except CvBridgeError as e:
+                print(e)
+
+            rate.sleep()
+
+        return color_img, depth_img
+
+
+    def get_data(self):
+
+        # Ping the server with anything
+        self.tcp_socket.send(b'asdf')
+
+        # Fetch TCP data:
+        #     color camera intrinsics, 9 floats, number of bytes: 9 x 4
+        #     depth scale for converting depth from uint16 to float, 1 float, number of bytes: 4
+        #     depth image, self.im_width x self.im_height uint16, number of bytes: self.im_width x self.im_height x 2
+        #     color image, self.im_width x self.im_height x 3 uint8, number of bytes: self.im_width x self.im_height x 3
+        data = b''
+        while len(data) < (10*4 + self.im_height*self.im_width*5):
+            data += self.tcp_socket.recv(self.buffer_size)
+
+        # Reorganize TCP data into color and depth frame
+        self.intrinsics = np.fromstring(data[0:(9*4)], np.float32).reshape(3, 3)
+        depth_scale = np.fromstring(data[(9*4):(10*4)], np.float32)[0]
+        depth_img = np.fromstring(data[(10*4):((10*4)+self.im_width*self.im_height*2)], np.uint16).reshape(self.im_height, self.im_width)
+        color_img = np.fromstring(data[((10*4)+self.im_width*self.im_height*2):], np.uint8).reshape(self.im_height, self.im_width, 3)
+        depth_img = depth_img.astype(float) * depth_scale
+        
+
+        return color_img, depth_img
+
+
+'''
 class Camera(object):
 
     def __init__(self):
@@ -46,8 +150,14 @@ class Camera(object):
         depth_img = np.fromstring(data[(10*4):((10*4)+self.im_width*self.im_height*2)], np.uint16).reshape(self.im_height, self.im_width)
         color_img = np.fromstring(data[((10*4)+self.im_width*self.im_height*2):], np.uint8).reshape(self.im_height, self.im_width, 3)
         depth_img = depth_img.astype(float) * depth_scale
-        return color_img, depth_img
 
+        tmp_color_image = cv2.cvtColor(color_img, cv2.COLOR_RGB2BGR)
+        
+        #Displayed the image
+        cv2.imshow("tmp_color_image", tmp_color_image)
+
+        return color_img, depth_img
+'''
 
 
 # DEPRECATED CAMERA CLASS FOR REALSENSE WITH ROS
@@ -110,3 +220,10 @@ class Camera(object):
     def stop_recording(self):
         self.is_recording = False
 '''
+
+
+if __name__ == '__main__':
+    rospy.init_node('camera', anonymous=True)
+    camera = Camera()
+
+    rospy.spin()
